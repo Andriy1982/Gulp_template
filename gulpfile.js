3997f3902d6f954b1ed0a2a5ee002067a7653f50
Andrii Krychkivskiy <kruchandriy@gmail.com>
2022-08-07 22:54:38 +0300

    add new plugins for html and css

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,25 @@ import {path} from './gulp/config/path.js';
 
 import {copy} from './gulp/tasks/copy.js';
 import {reset} from './gulp/tasks/reset.js';
+import {html} from './gulp/tasks/html.js';
+import { plugins } from './gulp/config/plugins.js';
+import {server} from './gulp/tasks/server.js';
+import {scss} from './gulp/tasks/scss.js';
 
-const dev = gulp.series(reset, copy, watcher);
+const mainTasks = gulp.parallel(copy, html, scss);
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 global.app = {
     path,
     gulp,
+    plugins
 }
 
 function watcher() {
     gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.html, html)
+    gulp.watch(path.watch.scss, scss)
 }
 
 gulp.task('default', dev);
